1af9e688dbe59d219c54f9488ab1efce1659dc1e
astranono <90936000+astranono@users.noreply.github.com>
2025-12-09 22:31:57 -0500

    finished project

--- src/Book.java
+++ src/Book.java
@@ -2,13 +2,14 @@ import java.sql.SQLOutput;
 
 public class Book {
     
-    public int rating;
-    public String title;
-    public String author;
+    private int rating;
+    private String title;
+    private String author;
     
     public Book(int pRating){
-        title = "Grandmaster of Demonic Cultivation";
-        author = "MO Xiang Tong Xiu";
+        title = "Flash Boys: A Wall Street Revolt";
+        author = "Michael Lewis";
+        this.rating = pRating;
         // TODO: set the instance variable to the parameter
 
     }
@@ -16,11 +17,37 @@ public class Book {
     public void print(){
         System.out.println("title: " + title);
         System.out.println("author: " + author);
+        System.out.println("rating: " + rating);
         //TODO: use System.out to print the values of the
         //      instance varibles so the user can see the review
 
 
     }
+    //getters
+    //also should I be doing camal case or pascal for varibles
+    public int getRating() {
+        return rating;
+    }
 
-    
+    // 2. Getter for title
+    public String getTitle() {
+        return title;
+    }
+
+    // 3. Getter for author
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
 }
--- src/Library.java
+++ src/Library.java
@@ -2,10 +2,11 @@ import java.sql.SQLOutput;
 
 public class Library {
 
-    public String owner = "Mx. Bradford";
-    public Book[] allBooks;
+    public String owner = "Noah Farkas";
+    //size of 10
+    public Book[] allBooks = new Book[10];
     public static void main(String[] args) {
-        //TODO: Make an instance of the main class
+        new Library();
     }
 
     public Library(){
@@ -14,15 +15,39 @@ public class Library {
         //TODO: Give the array allBooks a size of 10
 
         //TODO: Fill the array allBooks with reviews with random ratings from 0 to 10 inclusive
+        allBooks[0] = new Book(0);
+        allBooks[1] = new Book(1);
+        allBooks[2] = new Book(2);
+        allBooks[3] = new Book(3);
+        allBooks[4] = new Book(4);
+        allBooks[5] = new Book(5);
+        allBooks[6] = new Book(6);
+        allBooks[7] = new Book(7);
+        allBooks[8] = new Book(8);
+        allBooks[9] = new Book(9);
 
         //TODO: print all the ratings in the array allBooks
-        
-        //TODO: Call the method averageRating
-
+        for(int i = 0; i<allBooks.length; i++){
+            System.out.println(allBooks[i].getRating());
+        }
+        //TODO: Call the method averageRating I don't think I really need to use setters, as I am not altering the data
+        averageRating();
     }
 
     public void averageRating(){
         //TODO: calculate the average rating from the array allBooks and print it
+        int computation = 0;
+        //averages tend to be decimals, my fisrt output was wrong
+        double averageRating = 0;
+
+        for(int i = 0; i<allBooks.length; i++){
+            computation += allBooks[i].getRating();
+            if (i == allBooks.length-1){
+                averageRating = (double) computation / allBooks.length;
+                System.out.println("Average rating is : " + averageRating);
+            }
+
+        }
 
     }
 }
